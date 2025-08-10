72d564f684d2a8ad809712b1305b1006ea7190dd
Yugal Lakhyani <lakhyani.y@northeastern.edu>
2025-08-10 14:58:07 -0400

    Add commented code

--- week5.js
+++ week5.js
@@ -8,7 +8,28 @@ console.log(popped)
 arr.push(10);
 console.log(arr)
 
+// console.log(arr.length)
+// let swapped = arr.splice(1, 3);
 
+// arr.splice()
+
+// console.log(arr)
+// console.log(swapped);
+// console.log(arr.length)
+
+// console.log(arr[1])
+
+
+//Difference between array and string
+// const arrOfChar = ['a', 'b', 'c']
+
+// console.log(arrOfChar)
+// console.log(arrOfChar[0])
+
+// const dem = 'abc';
+
+// console.log(dem)
+// console.log(dem[0])
 
 
 
